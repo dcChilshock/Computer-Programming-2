1e1341dd26fe82657b71955b074ecbaab7d5cee5
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-01-23 19:55:41 +0000

    prog54b

--- Prog54b.py
+++ Prog54b.py
@@ -1,4 +1,8 @@
-Number1 = int(intput("enter number1(3 digits) ="))
-Number2 = int(intput("enter number2(3 digits) ="))
-Number3 = int(intput("enter number3(3 digits) ="))
-Number4 = int(intput("enter number4(3 digits) ="))
+Number1 = int(input("enter number1(3 digits) = "))
+Number2 = int(input("enter number2(3 digits) = "))
+Number3 = int(input("enter number3(3 digits) = "))
+Number4 = int(input("enter number4(3 digits) = "))
+sum = Number1 + Number2 + Number3 + Number4 
+average = sum / 4
+print("The Sum of the 4 numbers are: " + str(sum))
+print("The Average of the 4 numbers is: " + str(average))
