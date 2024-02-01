89ca9dbcc68f5cc53a47994ab72967c0157e2854
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-02-01 20:14:56 +0000

    LP4-9 IS COMPLETE

--- lp4-9.py
+++ lp4-9.py
@@ -2,5 +2,10 @@ import random
 
 rnd = random.randint(1,20)
 guess = int(input("Enter your guess 1-20, player: "))
+print("Computers Number: " + str(rnd))
+print("Players Number: " + str(guess)) 
 if rnd == guess:
-  print("You won, the secret number was: " + str(rnd) + " and your guess was: " + str(guess))
+  print("You won")
+else:
+  print("Better luck next time loser.")
+  
