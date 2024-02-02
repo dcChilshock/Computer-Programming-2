80a8c8be9ace6d68608626ca62731804f0a5f9fb
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-02-02 20:29:54 +0000

    lp5-10 is done

--- lp5-10.py
+++ lp5-10.py
@@ -1,5 +1,7 @@
-num1 = int(input("Input Number 1: "))
-num2 = int(input("Input Number 2: "))
-temp = num1  num2
-gcd = temp * 10 
-print("The GCD is: " + str(gcd))
+num1 = int(input("Input bigger number 1: "))
+num2 = int(input("Input smaller number 2: "))
+while num2 > 0:
+  temp = num1 % num2 
+  num1 = num2
+  num2 = temp
+print("The greatest common factor is: " + str(num1))
