5e726ffe62528c1ad542b98175af95704dc3230e
dcChilshock <densonchilders123@gmail.com>
2024-02-14 14:39:09 -0600

    tried string thing 3 feb 14

--- Strint3Try3/Strint3Try3/MainForm.py
+++ Strint3Try3/Strint3Try3/MainForm.py
@@ -24,7 +24,7 @@ class MainForm(Form):
 		self._button1.Name = "button1"
 		self._button1.Size = System.Drawing.Size(75, 23)
 		self._button1.TabIndex = 0
-		self._button1.Text = "button1"
+		self._button1.Text = "get letter"
 		self._button1.UseVisualStyleBackColor = True
 		self._button1.Click += self.Button1Click
 		# 
@@ -34,8 +34,9 @@ class MainForm(Form):
 		self._button2.Name = "button2"
 		self._button2.Size = System.Drawing.Size(75, 23)
 		self._button2.TabIndex = 1
-		self._button2.Text = "button2"
+		self._button2.Text = "clear"
 		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
@@ -43,8 +44,9 @@ class MainForm(Form):
 		self._button3.Name = "button3"
 		self._button3.Size = System.Drawing.Size(75, 23)
 		self._button3.TabIndex = 2
-		self._button3.Text = "button3"
+		self._button3.Text = "exit"
 		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
 		# 
 		# label1
 		# 
@@ -52,27 +54,27 @@ class MainForm(Form):
 		self._label1.Name = "label1"
 		self._label1.Size = System.Drawing.Size(100, 23)
 		self._label1.TabIndex = 3
-		self._label1.Text = "label1"
+		self._label1.Text = "Insert word:"
 		# 
 		# label2
 		# 
-		self._label2.Location = System.Drawing.Point(34, 247)
+		self._label2.Location = System.Drawing.Point(180, 172)
 		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(100, 23)
+		self._label2.Size = System.Drawing.Size(225, 151)
 		self._label2.TabIndex = 4
-		self._label2.Text = "label2"
 		# 
 		# label3
 		# 
-		self._label3.Location = System.Drawing.Point(225, 247)
+		self._label3.Location = System.Drawing.Point(34, 172)
 		self._label3.Name = "label3"
-		self._label3.Size = System.Drawing.Size(100, 23)
+		self._label3.Size = System.Drawing.Size(149, 40)
 		self._label3.TabIndex = 5
-		self._label3.Text = "label3"
+		self._label3.Text = "First letter of inserted word:"
+		self._label3.Click += self.Label3Click
 		# 
 		# textBox1
 		# 
-		self._textBox1.Location = System.Drawing.Point(140, 122)
+		self._textBox1.Location = System.Drawing.Point(155, 116)
 		self._textBox1.Name = "textBox1"
 		self._textBox1.Size = System.Drawing.Size(100, 20)
 		self._textBox1.TabIndex = 6
@@ -101,15 +103,17 @@ class MainForm(Form):
 		string = string.lower()
 		lcv = 0
 		lcv2 = 1
-		letter1 = string[lcv:]
+		letter1 = string[0:1]
 		count = 0
-		while len(string) <= lcv3:
-			letter2 = string[lcv2:lcv2+1] #so selects 'o' out of morning then 'r' and so on
-			if letter1 == letter2: #if current letter == second letter move on to the next letters 
-				lcv2 += 2
-				lcv += 2
-				count +=1
-			else:
-				lcv == 0
-		if count == 
-		self._label3.Text = "This letter: " + letter1
+		self._label2.Text = letter1
+
+	def Button2Click(self, sender, e):
+		self._label3.Text = " " 
+		self._label2.Text = " " 
+		self.textBox1.Text = " " 
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+
+	def Label3Click(self, sender, e):
+		pass
