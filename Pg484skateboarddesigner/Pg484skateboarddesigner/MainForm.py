51b4ab1782b8fd10ea89e699254a8cbdfbf1cf8b
dcChilshock <densonchilders123@gmail.com>
2024-03-05 14:35:45 -0600

    dbdbdgfbfd

---
+++ Pg484skateboarddesigner/Pg484skateboarddesigner/MainForm.py
@@ -0,0 +1,230 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._checkBox1 = System.Windows.Forms.CheckBox()
+		self._checkBox2 = System.Windows.Forms.CheckBox()
+		self._checkBox3 = System.Windows.Forms.CheckBox()
+		self._checkBox4 = System.Windows.Forms.CheckBox()
+		self._checkBox5 = System.Windows.Forms.CheckBox()
+		self._checkBox6 = System.Windows.Forms.CheckBox()
+		self._checkBox7 = System.Windows.Forms.CheckBox()
+		self._checkBox8 = System.Windows.Forms.CheckBox()
+		self._checkBox9 = System.Windows.Forms.CheckBox()
+		self._checkBox10 = System.Windows.Forms.CheckBox()
+		self._checkBox11 = System.Windows.Forms.CheckBox()
+		self._checkBox12 = System.Windows.Forms.CheckBox()
+		self._checkBox13 = System.Windows.Forms.CheckBox()
+		self._checkBox14 = System.Windows.Forms.CheckBox()
+		self._checkBox15 = System.Windows.Forms.CheckBox()
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# checkBox1
+		# 
+		self._checkBox1.Location = System.Drawing.Point(12, 61)
+		self._checkBox1.Name = "checkBox1"
+		self._checkBox1.Size = System.Drawing.Size(181, 24)
+		self._checkBox1.TabIndex = 0
+		self._checkBox1.Text = "The Master Thrasher -- $60"
+		self._checkBox1.UseVisualStyleBackColor = True
+		# 
+		# checkBox2
+		# 
+		self._checkBox2.Location = System.Drawing.Point(12, 89)
+		self._checkBox2.Name = "checkBox2"
+		self._checkBox2.Size = System.Drawing.Size(181, 24)
+		self._checkBox2.TabIndex = 1
+		self._checkBox2.Text = """The Dictator of the Grind -- $45
+"""
+		self._checkBox2.UseVisualStyleBackColor = True
+		# 
+		# checkBox3
+		# 
+		self._checkBox3.Location = System.Drawing.Point(13, 119)
+		self._checkBox3.Name = "checkBox3"
+		self._checkBox3.Size = System.Drawing.Size(180, 27)
+		self._checkBox3.TabIndex = 2
+		self._checkBox3.Text = """The Street King -- $50
+
+
+"""
+		self._checkBox3.UseVisualStyleBackColor = True
+		# 
+		# checkBox4
+		# 
+		self._checkBox4.Location = System.Drawing.Point(13, 168)
+		self._checkBox4.Name = "checkBox4"
+		self._checkBox4.Size = System.Drawing.Size(104, 24)
+		self._checkBox4.TabIndex = 3
+		self._checkBox4.Text = "7.75 axle"
+		self._checkBox4.UseVisualStyleBackColor = True
+		# 
+		# checkBox5
+		# 
+		self._checkBox5.Location = System.Drawing.Point(12, 198)
+		self._checkBox5.Name = "checkBox5"
+		self._checkBox5.Size = System.Drawing.Size(104, 24)
+		self._checkBox5.TabIndex = 4
+		self._checkBox5.Text = "8 axle"
+		self._checkBox5.UseVisualStyleBackColor = True
+		# 
+		# checkBox6
+		# 
+		self._checkBox6.Location = System.Drawing.Point(12, 228)
+		self._checkBox6.Name = "checkBox6"
+		self._checkBox6.Size = System.Drawing.Size(104, 24)
+		self._checkBox6.TabIndex = 5
+		self._checkBox6.Text = "8.5 axle"
+		self._checkBox6.UseVisualStyleBackColor = True
+		# 
+		# checkBox7
+		# 
+		self._checkBox7.Location = System.Drawing.Point(12, 281)
+		self._checkBox7.Name = "checkBox7"
+		self._checkBox7.Size = System.Drawing.Size(104, 24)
+		self._checkBox7.TabIndex = 6
+		self._checkBox7.Text = "51 mm"
+		self._checkBox7.UseVisualStyleBackColor = True
+		# 
+		# checkBox8
+		# 
+		self._checkBox8.Location = System.Drawing.Point(13, 311)
+		self._checkBox8.Name = "checkBox8"
+		self._checkBox8.Size = System.Drawing.Size(104, 24)
+		self._checkBox8.TabIndex = 7
+		self._checkBox8.Text = "55 mm"
+		self._checkBox8.UseVisualStyleBackColor = True
+		# 
+		# checkBox9
+		# 
+		self._checkBox9.Location = System.Drawing.Point(12, 341)
+		self._checkBox9.Name = "checkBox9"
+		self._checkBox9.Size = System.Drawing.Size(104, 24)
+		self._checkBox9.TabIndex = 8
+		self._checkBox9.Text = "58 mm "
+		self._checkBox9.UseVisualStyleBackColor = True
+		# 
+		# checkBox10
+		# 
+		self._checkBox10.Location = System.Drawing.Point(12, 371)
+		self._checkBox10.Name = "checkBox10"
+		self._checkBox10.Size = System.Drawing.Size(104, 24)
+		self._checkBox10.TabIndex = 9
+		self._checkBox10.Text = "61 mm"
+		self._checkBox10.UseVisualStyleBackColor = True
+		# 
+		# checkBox11
+		# 
+		self._checkBox11.Location = System.Drawing.Point(240, 269)
+		self._checkBox11.Name = "checkBox11"
+		self._checkBox11.Size = System.Drawing.Size(104, 24)
+		self._checkBox11.TabIndex = 10
+		self._checkBox11.Text = "checkBox11"
+		self._checkBox11.UseVisualStyleBackColor = True
+		# 
+		# checkBox12
+		# 
+		self._checkBox12.Location = System.Drawing.Point(240, 299)
+		self._checkBox12.Name = "checkBox12"
+		self._checkBox12.Size = System.Drawing.Size(104, 24)
+		self._checkBox12.TabIndex = 11
+		self._checkBox12.Text = "checkBox12"
+		self._checkBox12.UseVisualStyleBackColor = True
+		# 
+		# checkBox13
+		# 
+		self._checkBox13.Location = System.Drawing.Point(240, 329)
+		self._checkBox13.Name = "checkBox13"
+		self._checkBox13.Size = System.Drawing.Size(104, 24)
+		self._checkBox13.TabIndex = 12
+		self._checkBox13.Text = "checkBox13"
+		self._checkBox13.UseVisualStyleBackColor = True
+		# 
+		# checkBox14
+		# 
+		self._checkBox14.Location = System.Drawing.Point(240, 359)
+		self._checkBox14.Name = "checkBox14"
+		self._checkBox14.Size = System.Drawing.Size(104, 24)
+		self._checkBox14.TabIndex = 13
+		self._checkBox14.Text = "checkBox14"
+		self._checkBox14.UseVisualStyleBackColor = True
+		# 
+		# checkBox15
+		# 
+		self._checkBox15.Location = System.Drawing.Point(240, 389)
+		self._checkBox15.Name = "checkBox15"
+		self._checkBox15.Size = System.Drawing.Size(104, 24)
+		self._checkBox15.TabIndex = 14
+		self._checkBox15.Text = "checkBox15"
+		self._checkBox15.UseVisualStyleBackColor = True
+		# 
+		# label1
+		# 
+		self._label1.Location = System.Drawing.Point(13, 13)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(135, 16)
+		self._label1.TabIndex = 15
+		self._label1.Text = "SkateBoard designer."
+		# 
+		# label2
+		# 
+		self._label2.Location = System.Drawing.Point(13, 43)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(44, 15)
+		self._label2.TabIndex = 16
+		self._label2.Text = "Decks:"
+		# 
+		# label3
+		# 
+		self._label3.Location = System.Drawing.Point(12, 149)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(100, 16)
+		self._label3.TabIndex = 17
+		self._label3.Text = """Truck assemblies:
+"""
+		# 
+		# label4
+		# 
+		self._label4.Location = System.Drawing.Point(12, 259)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(100, 23)
+		self._label4.TabIndex = 18
+		self._label4.Text = "Wheel sets:"
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(965, 447)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._checkBox15)
+		self.Controls.Add(self._checkBox14)
+		self.Controls.Add(self._checkBox13)
+		self.Controls.Add(self._checkBox12)
+		self.Controls.Add(self._checkBox11)
+		self.Controls.Add(self._checkBox10)
+		self.Controls.Add(self._checkBox9)
+		self.Controls.Add(self._checkBox8)
+		self.Controls.Add(self._checkBox7)
+		self.Controls.Add(self._checkBox6)
+		self.Controls.Add(self._checkBox5)
+		self.Controls.Add(self._checkBox4)
+		self.Controls.Add(self._checkBox3)
+		self.Controls.Add(self._checkBox2)
+		self.Controls.Add(self._checkBox1)
+		self.Name = "MainForm"
+		self.Text = "Pg484skateboarddesigner"
+		self.ResumeLayout(False)
+
