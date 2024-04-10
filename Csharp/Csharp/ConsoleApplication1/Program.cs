65b99060f774f9f183a581203dcb1cdd328ea747
dcChilshock <densonchilders123@gmail.com>
2024-04-10 14:16:27 -0500

    jfgh

--- Csharp/Csharp/ConsoleApplication1/Program.cs
+++ Csharp/Csharp/ConsoleApplication1/Program.cs
@@ -10,7 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Enter Length: ");
+            int len = int.Parse(Console.ReadLine());
+            Console.Write("Enter Width: ");
+            int wid = int.Parse(Console.ReadLine());
+            int area = len * wid;
+            int perim = 2 * len + 2 * wid;
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perim);
+            Console.ReadKey();
         }
     }
 }
