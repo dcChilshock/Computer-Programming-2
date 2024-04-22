98f50a97ceb13f0d7a5d26a408b178223cf2efe8
dcChilshock <densonchilders123@gmail.com>
2024-04-22 14:36:33 -0500

    54bform like 80% complete

---
+++ Csharp/Csharp/54bForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _54bForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            label3.Text = " ";
+            label4.Text = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int num3 = int.Parse(textBox3.Text);
+            int num4 = int.Parse(textBox4.Text);
+            int sum = num1 + num2 + num3 + num4;
+            double average = sum / 4.00;
+        }
+    }
+}
