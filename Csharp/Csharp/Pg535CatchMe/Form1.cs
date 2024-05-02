1c13ecf5dc7f085436fc74bdcd522c5f2e95ea0a
dcChilshock <densonchilders123@gmail.com>
2024-05-02 14:30:55 -0500

    pg237 mass and weight

--- Csharp/Csharp/Pg535CatchMe/Form1.cs
+++ Csharp/Csharp/Pg535CatchMe/Form1.cs
@@ -16,5 +16,28 @@ namespace Pg535CatchMe
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            label4.Text = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mass = int.Parse(textBox1.Text);
+            double weight = mass * 9.8;
+            if (mass < 0)
+                weight = 0;
+            if (weight == 0)
+                label4.Text = "Error zero or negative number not able to compute";
+            else
+                label4.Text = weight.ToString();
+        }
     }
 }
