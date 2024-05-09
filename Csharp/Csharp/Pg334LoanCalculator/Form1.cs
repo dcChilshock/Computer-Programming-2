d76fbc600bcfec3d47e68259272afae8e7c8c7a1
dcChilshock <densonchilders123@gmail.com>
2024-05-09 14:34:11 -0500

    stuff

--- Csharp/Csharp/Pg334LoanCalculator/Form1.cs
+++ Csharp/Csharp/Pg334LoanCalculator/Form1.cs
@@ -16,5 +16,38 @@ namespace Pg334LoanCalculator
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBox4.Text = " ";
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double c = double.Parse(textBox1.Text);
+            double d = double.Parse(textBox1.Text);
+            int t = int.Parse(textBox3.Text);
+            double i = double.Parse(textBox4.Text);
+            double 
+        }
     }
 }
