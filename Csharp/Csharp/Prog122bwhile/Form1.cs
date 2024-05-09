496512baee6470006285aa72d4e87eb038f2555c
dcChilshock <densonchilders123@gmail.com>
2024-05-09 14:17:35 -0500

    stuff

--- Csharp/Csharp/Prog122bwhile/Form1.cs
+++ Csharp/Csharp/Prog122bwhile/Form1.cs
@@ -16,5 +16,26 @@ namespace Prog122bwhile
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Add("Hours Pay");
+            for (int x = 0; x <= 40; x++)
+            {
+                int eq = x * 4;
+                listBox1.Items.Add(x.ToString() + "\t" + eq.ToString());
+            }
+        }
     }
 }
